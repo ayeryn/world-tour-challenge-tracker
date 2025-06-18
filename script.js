af71df002cb2978472103bd1e714bdb881256d8d
Eryn Li <erynli@utexas.edu>
2025-06-18 17:22:39 -0500

    update form styling; add mockData for books

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import { initMap } from "./map.js";
 import { createCountrySelect } from "./selector.js";
 import { handleButtons } from "./buttons.js";
+import { getBooks } from "./books.js";
 
 initMap();
+getBooks();
 handleButtons();
 createCountrySelect();
