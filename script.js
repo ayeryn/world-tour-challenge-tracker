f654bdfb2c764dda96abeb1e0d88e374da42c4a9
Eryn Li <erynli@utexas.edu>
2025-06-18 15:48:32 -0500

    add some button logic

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const map = L.map("map").setView([20, 0], 2); // Lat, Lng, Zoom (global view)
 
-// Add OpenStreetMap tile layer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors",
-}).addTo(map);
-
-function addBook(){
-  
+function initMap() {
+  // Add OpenStreetMap tile layer
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors",
+  }).addTo(map);
 }
 
-const handleClick = () => {
-  console.log("Test");
-};
+// Call all necessary functions here
+initMap();
+const showFormBtn = document.getElementById("show-form-btn");
+const hideFormBtn = document.getElementById("hide-form-btn");
+const bookForm = document.getElementById("book-form");
+showFormBtn.addEventListener("click", () => {
+  /*
+   * When Add Book is clicked:
+   * 1. Add Book becomes disabled
+   * 2. Cancel appears
+   * 3. Form appears
+   */
+  showFormBtn.disabled = true;
+  hideFormBtn.hidden = false;
+  bookForm.hidden = false;
+});
+
+hideFormBtn.addEventListener("click", () => {
+  /*
+   * When Cancel is clicked:
+   * 1. Add Book becomes available
+   * 2. Cancel disappears
+   * 3. Form clears and disappears
+   */
+  showFormBtn.disabled = false;
+  hideFormBtn.hidden = true;
+  bookForm.reset();
+  bookForm.hidden = true;
+});
